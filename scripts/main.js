df41b3986676b14a79e42bdc12dbfb06c659a7ee
Ivan <ivanmp@live.com>
2015-09-30 11:59:27 -0600

    update index and js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,15 @@
+var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h2');
-myHeading.innerHTML = 'fib:1123581321';
+
+function fib() {
+  myHeading.innerHTML = 'fib:1123581321';    
+}
+
+function getString() {
+  var myString = prompt('Please enter a string.');
+  myHeading.innerHTML = myString;
+}
+
+myButton.onclick = function() {
+  getString();
+}
